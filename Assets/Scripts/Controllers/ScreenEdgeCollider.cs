2204c49ace36ed1ca7884feecba3a4b1f47ced97
xloderunnerx <xloderunnerx@gmail.com>
2019-09-04 13:30:32 +0300

    Controllable ship

---
+++ Assets/Scripts/Controllers/ScreenEdgeCollider.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ScreenEdgeCollider : MonoBehaviour
+{
+
+    public List<EdgeCollider2D> _edges;
+
+    void Start()
+    {
+        _edges = new List<EdgeCollider2D>();
+        for (int i = 0; i < 4; i++)
+        {
+            gameObject.AddComponent<EdgeCollider2D>();
+        }
+        UpdateColliders();
+    }
+
+    void Update()
+    {
+        UpdateColliders();
+    }
+
+    void UpdateColliders()
+    {
+
+        Vector2 delta = Camera.main.transform.position - transform.position;
+        List<EdgeCollider2D> edgeColliders = GetComponents<EdgeCollider2D>().ToList();
+
+        edgeColliders[0].points = new Vector2[] { delta + GetWorldViewPort(new Vector2(0, 0)), delta + GetWorldViewPort(new Vector2(0, 1)) };
+        edgeColliders[1].points = new Vector2[] { delta + GetWorldViewPort(new Vector2(0, 1)), delta + GetWorldViewPort(new Vector2(1, 1)) };
+        edgeColliders[2].points = new Vector2[] { delta + GetWorldViewPort(new Vector2(1, 1)), delta + GetWorldViewPort(new Vector2(1, 0)) };
+        edgeColliders[3].points = new Vector2[] { delta + GetWorldViewPort(new Vector2(1, 0)), delta + GetWorldViewPort(new Vector2(0, 0)) };
+        _edges.Clear();
+        _edges = edgeColliders;
+    }
+
+    Vector2 GetWorldViewPort(Vector2 normalizedPos)
+    {
+        return Camera.main.ViewportToWorldPoint(new Vector3(normalizedPos.x, normalizedPos.y, Camera.main.nearClipPlane));
+    }
+
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    [SerializeField]
+    private float _horizontalSpeed;
+
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        MoveHorizontal();
+    }
+
+    private void MoveHorizontal()
+    {
+        _rigidbody2D.velocity = new Vector2(Input.GetAxis("Horizontal") * _horizontalSpeed, _rigidbody2D.velocity.y);
+    }
+}
