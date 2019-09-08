73ddab549d9423de02eb809c103bc3cad886a66e
xloderunnerx <xloderunnerx@gmail.com>
2019-09-08 17:02:44 +0300

    Hud Close button

--- Assets/Scripts/UI/CloseButton.cs
+++ Assets/Scripts/UI/CloseButton.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CloseButton : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(Close);   
+    }
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void Close()
     {
-        
+        Application.Quit();
     }
 }
